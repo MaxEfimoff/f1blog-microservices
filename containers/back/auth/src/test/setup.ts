8ddf4d93546de8732229597c28dc3924903f4fa4
Максим Ефимов <maksimefimov@a179.sub177.net78.udm.net>
2021-03-07 19:42:21 +0400

    updated test setup file for auth service

--- containers/back/auth/src/test/setup.ts
+++ containers/back/auth/src/test/setup.ts
@@ -1,46 +1,63 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
+// import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
-import { users } from '../routes/api/users/users';
+// import { app } from '../app';
+// import { users } from '../routes/api/users/users';
 
-let mongo: any;
+// let mongo: any;
 
 jest.mock('../nats-wrapper');
 
 beforeAll(async () => {
-  process.env.JWT_KEY = 'yjdtjd';
+  // process.env.JWT_KEY = 'yjdtjd';
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
-
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-});
-
-beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections();
-
-  for (let collection of collections) {
-    await collection.deleteMany({});
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_Key not set');
   }
 
-  const db = await mongoose.connection.db;
+  if (!process.env.MONGO_AUTH_KEY) {
+    throw new Error('MONGO_AUTH_KEY not set');
+  }
 
-  await db.dropCollection('users');
-  const usersColl = await db.createCollection('users');
+  const db = process.env.MONGO_AUTH_KEY;
 
-  await usersColl.insertOne({
-    active: true,
-    role: 'guest',
-    name: 'Max',
-    email: 'emv3@ya.ru',
-    password: '$2b$10$8zemadXxMLUwpf38PyZGDOMeeYr2d1qv5hEduzRx4Ex12xyYIUzmy',
+  await mongoose.connect(db, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
   });
+
+  // mongo = new MongoMemoryServer();
+  // const mongoUri = await mongo.getUri();
+
+  // await mongoose.connect(mongoUri, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
 });
 
-afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
-});
+// beforeEach(async () => {
+//   const collections = await mongoose.connection.db.collections();
+
+//   for (let collection of collections) {
+//     await collection.deleteMany({});
+//   }
+
+//   const db = await mongoose.connection.db;
+
+//   await db.dropCollection('users');
+//   const usersColl = await db.createCollection('users');
+
+//   await usersColl.insertOne({
+//     active: true,
+//     role: 'guest',
+//     name: 'Max',
+//     email: 'emv3@ya.ru',
+//     password: '$2b$10$8zemadXxMLUwpf38PyZGDOMeeYr2d1qv5hEduzRx4Ex12xyYIUzmy',
+//   });
+// });
+
+// afterAll(async () => {
+//   await mongo.stop();
+//   await mongoose.connection.close();
+// });
