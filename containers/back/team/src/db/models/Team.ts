493de6954fc17a2d626abd48171ba847b8e14d02
Maxim Efimoff <emv3@ya.ru>
2021-05-04 07:04:29 +0400

    added team container

---
+++ containers/back/team/src/db/models/Team.ts
@@ -0,0 +1,104 @@
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import { ProfileDoc } from './Profile';
+import { GroupDoc } from './Group';
+
+// An interface that describes the properties
+// that are required to create a new Team
+interface TeamAttrs {
+  profile: ProfileDoc;
+  title: string;
+  createdAt: number;
+}
+
+// An interface that describes the properties
+// that a Team Model has
+interface TeamModel extends mongoose.Model<TeamDoc> {
+  build(attrs: TeamAttrs): TeamDoc;
+  findByEvent(event: { id: string; version: number }): Promise<TeamDoc | null>;
+}
+
+// An interface that describes the properties
+// that a single Team Document has
+export interface TeamDoc extends mongoose.Document {
+  profile: ProfileDoc;
+  title: string;
+  description?: string;
+  avatar?: string;
+  members?: ProfileDoc[];
+  groups?: GroupDoc[];
+  createdAt: number;
+  updatedAt?: number;
+  version: number;
+}
+
+const TeamSchema = new mongoose.Schema(
+  {
+    profile: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Profile',
+    },
+    title: {
+      type: String,
+      max: 200,
+    },
+    description: {
+      type: String,
+      max: 1200,
+    },
+    avatar: {
+      type: String,
+      max: 1200,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Profile',
+      },
+    ],
+    groups: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Group',
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+TeamSchema.set('versionKey', 'version');
+TeamSchema.plugin(updateIfCurrentPlugin);
+
+// Add findByEvent method to a TeamSchema
+TeamSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Team.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
+
+// Add build method to a TeamSchema
+TeamSchema.statics.build = (attrs: TeamAttrs) => {
+  return new Team(attrs);
+};
+
+const Team = mongoose.model<TeamDoc, TeamModel>('Team', TeamSchema);
+
+export { Team };
---
+++ containers/back/team/src/routes/controllers/teams/delete-team.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import 'express-async-errors';
+import { BadRequestError, NotFoundError } from '@f1blog/common';
+import { Profile } from '../../../db/models/Profile';
+import { Team } from '../../../db/models/Team';
+import { natsWrapper } from '../../../nats-wrapper';
+import { NotAuthorizedError } from '@f1blog/common';
+
+interface UserRequest extends Request {
+  user: {
+    id: string;
+    name: string;
+    isSuperadmin: boolean;
+    iat: number;
+    exp: number;
+  };
+}
+
+const deleteTeam = async (req: UserRequest, res: Response) => {
+  const user = req.user;
+
+  if (!user) {
+    throw new NotFoundError();
+  }
+
+  if (user.isSuperadmin == false) {
+    throw new BadRequestError('Only superadmin can delete a team');
+  }
+
+  const profile = await Profile.findOne({ user_id: req.user.id });
+
+  if (!profile) {
+    throw new BadRequestError('You should create profile first');
+  } else {
+    const team = await Team.findById(req.params.id);
+
+    if (team.profile.toString() !== profile.id) {
+      return new NotAuthorizedError();
+    }
+
+    await team.remove();
+
+    // Publish a TeamDeleted event
+    new TeamDeletedPublisher(natsWrapper.client).publish({
+      id: team.id,
+      title: null,
+      version: team.version,
+      profile_id: profile.id,
+    });
+
+    return res.status(201).json({
+      status: 'success',
+      data: {
+        team,
+      },
+    });
+  }
+};
+
+export { deleteTeam };
